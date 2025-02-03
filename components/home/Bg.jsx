38319533e53db546a349427633a09fd9dba7372a
Abdul Ghaffar <agscontact777@gmail.com>
2025-02-03 17:50:02 +0500

    Project setup

---
+++ components/home/Bg.jsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useEffect } from 'react'
+
+const Bg = () => {
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div className='absolute w-screen max-h-[600px] min-h-[33vh] bg-red-500'>yes</div>
+    )
+}
+
+export default Bg
---
+++ components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bg from './Bg'
+
+const Home = () => {
+    return (
+        <div>
+            <Bg />
+        </div>
+    )
+}
+
+export default Home
