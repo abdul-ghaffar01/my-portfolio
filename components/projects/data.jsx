a1e54ac833088c11e900da75a9986f6d81543150
Abdul Ghaffar <agscontact777@gmail.com>
2025-02-11 17:25:07 +0500

    Setuped sequence of projects

--- components/projects/data.jsx
+++ components/projects/data.jsx
@@ -1,34 +1,34 @@
-const projectsData = [
-    {
-        title: "Console based E-commerce store",
-        desc: "My Console-Based E-Commerce Store was my second-semester project, built entirely using pure Object-Oriented Programming (OOP) principles. It featured a modular design with classes for users, products, and carts, along with file handling for data persistence. The system allowed users to browse products, add them to the cart, and proceed with purchases, ensuring an interactive shopping experience within a terminal environment.",
-        img: "/projects/console-ecom.png",
-        tools: ["C++"],
-        link: "/static-projects/console/cbes"
-    }, {
-        title: "E-commerce store using Django",
-        desc: "A dynamic eCommerce platform built with Django, offering a seamless shopping experience with user authentication, cart management, and secure transactions. Designed for efficiency and scalability, it ensures smooth navigation and easy product browsing",
-        img: "/projects/ecom-using-django.png",
-        tools: ["Python", "Javascript", "Django"],
-        link: "/coming-soon"
-    }, {
-        title: "Database management system first semester",
-        desc: "Aughr is a console-based database management system (DBMS) designed to handle structured data operations efficiently. It supports basic CRUD (Create, Read, Update, Delete) functionalities, allowing users to store, query, and manage data through a command-line interface. Built with C++, Aughr focuses on simplicity, performance, and a structured approach to data handling.",
-        img: "/projects/dbms.png",
-        tools: ["C++"],
-        link: "/static-projects/console/dbms"
-    }, {
-        title: "Instagram home page clone",
-        desc: "I built an Instagram home page clone using Tailwind CSS, designed specifically for laptop screens. While it isn’t fully responsive, the layout is pixel-perfect, closely mirroring Instagram’s design with a clean and structured UI. The project showcases my ability to replicate modern interfaces with precision and attention to detail",
-        img: "/projects/instaclone.png",
-        tools: ["TailwindCss", "Html5"],
-        link: "/static-projects/insta-clone-using-tailwindcss"
-    }, {
-        title: "Facebook home page clone",
-        desc: "A static recreation of Facebook’s homepage built using Tailwind CSS. This clone replicates the classic Facebook layout, including the navbar, sidebar, news feed, and story section. While visually similar, it lacks responsiveness, meaning it works well on larger screens but does not adapt to smaller devices like phones or tablets. Ideal for exploring Tailwind styling and component structuring! 🚀",
-        img: "/projects/facebookclone.png",
-        tools: ["TailwindCss", "Html5"],
-        link: "/static-projects/facebook-clone-using-tailwindcss"
-    },
+const projectsData = [{
+    title: "Database management system first semester",
+    desc: "Aughr is a console-based database management system (DBMS) designed to handle structured data operations efficiently. It supports basic CRUD (Create, Read, Update, Delete) functionalities, allowing users to store, query, and manage data through a command-line interface. Built with C++, Aughr focuses on simplicity, performance, and a structured approach to data handling.",
+    img: "/projects/dbms.png",
+    tools: ["C++"],
+    link: "/static-projects/console/dbms"
+},
+{
+    title: "Console based E-commerce store",
+    desc: "My Console-Based E-Commerce Store was my second-semester project, built entirely using pure Object-Oriented Programming (OOP) principles. It featured a modular design with classes for users, products, and carts, along with file handling for data persistence. The system allowed users to browse products, add them to the cart, and proceed with purchases, ensuring an interactive shopping experience within a terminal environment.",
+    img: "/projects/console-ecom.png",
+    tools: ["C++"],
+    link: "/static-projects/console/cbes"
+}, {
+    title: "E-commerce store using Django",
+    desc: "A dynamic eCommerce platform built with Django, offering a seamless shopping experience with user authentication, cart management, and secure transactions. Designed for efficiency and scalability, it ensures smooth navigation and easy product browsing",
+    img: "/projects/ecom-using-django.png",
+    tools: ["Python", "Javascript", "Django"],
+    link: "/coming-soon"
+}, {
+    title: "Instagram home page clone",
+    desc: "I built an Instagram home page clone using Tailwind CSS, designed specifically for laptop screens. While it isn’t fully responsive, the layout is pixel-perfect, closely mirroring Instagram’s design with a clean and structured UI. The project showcases my ability to replicate modern interfaces with precision and attention to detail",
+    img: "/projects/instaclone.png",
+    tools: ["TailwindCss", "Html5"],
+    link: "/static-projects/insta-clone-using-tailwindcss"
+}, {
+    title: "Facebook home page clone",
+    desc: "A static recreation of Facebook’s homepage built using Tailwind CSS. This clone replicates the classic Facebook layout, including the navbar, sidebar, news feed, and story section. While visually similar, it lacks responsiveness, meaning it works well on larger screens but does not adapt to smaller devices like phones or tablets. Ideal for exploring Tailwind styling and component structuring! 🚀",
+    img: "/projects/facebookclone.png",
+    tools: ["TailwindCss", "Html5"],
+    link: "/static-projects/facebook-clone-using-tailwindcss"
+},
 ]
 export default projectsData
