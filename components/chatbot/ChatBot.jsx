e1ec5658a18ad4a4192bd45e652d6d8f9f6635cf
Abdul Ghaffar <agscontact777@gmail.com>
2025-07-26 11:13:37 +0500

    Added chat page and created simple ui  for chat

--- components/chatbot/ChatBot.jsx
+++ components/chatbot/ChatBot.jsx
@@ -1,40 +1,23 @@
 import Image from 'next/image'
 import MessageIcon from '@mui/icons-material/Message';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ChatWindow from './ChatWindow';
+import DownloadIcon from '@mui/icons-material/Download';
+import ClearIcon from '@mui/icons-material/Clear';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import { useRouter } from 'next/navigation';
 
 const ChatBot = () => {
-  // const [isChatOpened, setIsChatOpened] = useState(false)
-  const chatWindowElem = useRef(null)
-  const chatWindowElemArrow = useRef(null)
-  const showChat = () => {
-    if (chatWindowElem.current) {
-      chatWindowElem.current.style.height = "70vh"
-      chatWindowElem.current.style.width = "400px"
-      if (chatWindowElemArrow.current)
-        chatWindowElemArrow.current.style.borderTop = "12px solid var(--color-800)"
-    }
-  };
-  const hideChat = () => {
-    // Logic to close the chat window
-    console.log("Chat window closed");
-  };
-  return (
-    <div className="fixed bottom-0 right-0 z-50 flex flex-col overflow-hidden">
-      <div ref={chatWindowElem} className='w-screen h-screen bg-color-800 md:h-0 md:w-0 md:mr-3 md:rounded-md shadow-lg '>
-        {/* header of chat window */}
-        
-        <ChatWindow />
-      </div>
-      <div className='self-end mr-3 mb-3 flex flex-col items-center justify-center'>
-        {/* Pointing triangle */}
-        <div ref={chatWindowElemArrow} class="w-0 h-0 mb-1 border-l-8 border-r-8 border-l-transparent border-r-transparent border-t-0 border-t-color-800"></div>
+  const router = useRouter();
 
-        {/* Message icon button */}
-        <button onClick={showChat} className="w-fit self-end bg-color-700 text-light p-3 rounded-full shadow-md hover:bg-color-800 transition duration-300">
-          <MessageIcon className="text-2xl text-color-light" />
-        </button>
-      </div>
+  const handleClick = () => {
+    router.push("/chat")
+  }
+  return (
+    <div className='fixed bottom-3 right-3 w-12 h-12 shadow-lg bg-color-700 rounded-full right-0 z-50'>
+      <button className='w-full h-full flex items-center justify-center' onClick={handleClick}>
+        <MessageIcon className='text-color-light' />
+      </button>
     </div>
   )
 }
--- components/chatbot/ChatWindow.jsx
+++ components/chatbot/ChatWindow.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const ChatWindow = () => {
   return (
-    <div>ChatWindow</div>
+      <div ref={chatWindowElem} className='w-screen h-screen md:relative bg-color-800 md:h-0 md:w-0 md:mr-3 md:rounded-md shadow-lg overflow-hidden transition duration-300' >
+        {/* header of chat window */}
+        <div className='w-full h-[60px] p-2 flex items-center justify-between' >
+          {/* My name */}
+          <h3 className='text-color-light font-bold md:text-lg text-md' > Abdul Ghaffar - Bot</h3>
+
+          {/* right buttons */}
+          <div >
+            <button className='ml-2 text-color-light hover:text-color-400 transition duration-300'>
+              <DownloadIcon />
+            </button>
+            <button className='ml-2 text-color-light hover:text-color-400 transition duration-300'>
+              <FullscreenIcon />
+            </button>
+            <button onClick={hideChat} className='ml-2 text-color-light hover:text-color-400 transition duration-300'>
+              <ClearIcon />
+            </button>
+
+          </div >
+        </div >
+
+        {/* Bottom part of chat window */}
+        <div className='px-2 h-[calc(100%-68px)] rounded-md' >
+          {/* Main messeging window */}
+          {/* < ChatWindow /> */}
+        </div>
+
+      </div >
   )
 }
 
