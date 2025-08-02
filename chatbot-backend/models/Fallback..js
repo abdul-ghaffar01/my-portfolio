d1cc2b715f1e0eb8c8f1cd43e00d528b049400b8
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-02 20:26:38 +0500

    Removed chatbot-backend and created separated repo

--- chatbot-backend/models/Fallback..js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const fallbackSchema = new mongoose.Schema({
-  responses: [{ type: String, required: true }]   // e.g., ["I'm not sure, can you rephrase?"]
-});
-
-export default mongoose.model('Fallback', fallbackSchema);
--- chatbot-backend/utils/generate_jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const payload = {};  // empty payload
-
-const token = jwt.sign(payload, process.env.JWT_SECRET_BOT);
-
-console.log("Generated Token:", token);
