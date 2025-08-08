18332633734f9ce346d2eca7cc5b554c4a83ffa1
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-08 18:24:27 +0500

    Updated resume layout.js

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,23 +1,12 @@
-import Link from "next/link";
-import DownloadIcon from '@mui/icons-material/Download';
-
-export const metadata = {
-    title: "Abdul Ghaffar - Resume",
-    icons: "/favicon.png",
-};
-
 export default function Resume() {
     return (
-        <div>
-            {/* Download button  */}
-            {/* <Link download href='/files/Abdul Ghaffar - Resume.pdf' target="_blank" className="bg-purple-500 hover:bg-purple-700 p-2 rounded-md absolute bottom-3 left-1/2 transform -translate-x-1/2 font-bold text-white">
-                Download <DownloadIcon sx={{ color: "white" }} />
-            </Link> */}
-
-            <div>
-                <iframe src="/files/Abdul Ghaffar - Resume.pdf" className="w-screen h-screen"
-                    title="Resume" />
-            </div>
+        <div className="relative w-screen h-screen bg-gray-950">
+            {/* Resume preview */}
+            <iframe
+                src="/files/Abdul Ghaffar - Resume.pdf"
+                className="w-full h-full border-none"
+                title="Abdul Ghaffar Resume"
+            />
         </div>
-    )
+    );
 }
