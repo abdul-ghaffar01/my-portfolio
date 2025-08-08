879470a9cc6863a4de44228e40e2ca3a656cac19
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-08 17:22:43 +0500

    Added google analytics code

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,31 @@
 import "./globals.css";
-
+import Script from "next/script";
 
 export const metadata = {
   title: "Abdul Ghaffar",
   icons: "/favicon.ico",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        {```
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JTMJLBDLGG"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-JTMJLBDLGG');
-        </script>```}
+        {/* This will add GA in the head */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JTMJLBDLGG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JTMJLBDLGG');
+          `}
+        </Script>
       </head>
-      <body className="overflow-x-hidden">
-        {children}
-      </body>
+      <body className="overflow-x-hidden">{children}</body>
     </html>
   );
 }
