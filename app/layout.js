54838a54d3971ace516e95fd706d1808b1e0dfca
Abdul Ghaffar <agscontact777@gmail.com>
2025-08-08 17:20:37 +0500

    Added google analytics code

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JTMJLBDLGG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JTMJLBDLGG');
+        </script>
       </head>
       <body className="overflow-x-hidden">
         {children}
