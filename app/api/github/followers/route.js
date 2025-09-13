dba6b02c8549ec5665aa3c3fa709b0350d082892
Abdul Ghaffar <agscontact777@gmail.com>
2025-09-13 16:50:43 +0500

    Removed auth from github section in api

--- app/api/github/followers/route.js
+++ app/api/github/followers/route.js
@@ -1,10 +1,6 @@
 export async function GET(request) {
     try {
-        const profileData = await fetch("https://api.github.com/users/abdul-ghaffar01/followers", {
-            headers: {
-                Authorization: "token " + process.env.GITHUB_PAT
-            },
-        });
+        const profileData = await fetch("https://api.github.com/users/abdul-ghaffar01/followers");
         const data = await profileData.json()
         return new Response(JSON.stringify(data))
     } catch (error) {
